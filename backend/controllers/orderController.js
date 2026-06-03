b8e280a7bc5243f2194d975bb4cbb22d89f34b29
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-06-04 03:39:29 +0530

    Started working on Order model and controllers'

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,8 @@
+import  Order from "../models/orderModel.js";
+import  User from "../models/userModel.js";
+import  Product from "../models/productModel.js";
+
+import HandleError from "../utils/handleError.js";
+import handleAsyncError from "../middleware/handleAsyncError.js";
+
+// Creating new Order

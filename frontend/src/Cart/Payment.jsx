5ae947cfaaa1c4a6622a02031c0aa4b348d718a7
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-06-22 18:23:01 +0530

    completed payment page

--- frontend/src/Cart/Payment.jsx
+++ frontend/src/Cart/Payment.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../CartStyles/Payment.css'
+import PageTitle from '../components/PageTitle'
+import CheckoutPath from './CheckoutPath'
+import { Link } from 'react-router-dom'
 
 function Payment() {
+  const orderItem = JSON.parse(sessionStorage.getItem('orderItem'))
   return (
-    <div>Payment</div>
+    <>
+    <PageTitle title="Payment Processing"/>
+    <CheckoutPath activePath={2} />
+
+    <div className="payment-container">
+      <Link to='/order/confirm' className='payment-go-back' >Go Back</Link>
+      <button className="payment-btn">Pay {orderItem.total} /-</button>
+    </div>
+
+    </>
   )
 }
 

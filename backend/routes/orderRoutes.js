34e7a726aeef4ee9545750b312bd6b53f4402a80
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-06-26 20:11:45 +0530

    completed order summary

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,15 +5,17 @@ import { AllMyOrders, createNewOrder, deleteOrder, getAllOrders, getSingleOrder,
 const router = express.Router();
 
 router.route('/new/order').post(verifyUserAuth, createNewOrder);
+
+router.route('/order/:id')
+    .get(verifyUserAuth, getSingleOrder)
+
 router.route('/admin/order/:id')
-    .get(verifyUserAuth, roleBasedAccess('admin'), getSingleOrder)
     .put(verifyUserAuth, roleBasedAccess('admin'), updateOrderStatus)
     .delete(verifyUserAuth, roleBasedAccess('admin'), deleteOrder);
 
 router.route('/admin/orders')
     .get(verifyUserAuth, roleBasedAccess('admin'), getAllOrders)
     
-
 router.route('/orders/user').get(verifyUserAuth,AllMyOrders);
 
 export default router;

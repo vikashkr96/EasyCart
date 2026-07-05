96cd6af03e36e110b25fa4d62715cac5513f0b35
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-07-05 14:28:24 +0530

    final commit

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../componentStyles/Loader.css';
+import React from "react";
+import "../componentStyles/Loader.css";
 
 function Loader() {
   return (
-    <div className='loader-container'>
-        <div className="loader"></div>
+    <div className="loader-container">
+      <div className="loader"></div>
     </div>
-  )
+  );
 }
 
-export default Loader
+export default Loader;
